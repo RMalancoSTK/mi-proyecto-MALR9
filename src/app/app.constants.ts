a9085669bfa19ab5ef750eed4dbdeed6f5cbfc6e
Rafael Malanco <rafael.malanco@softtek.com>
2022-12-27 17:44:51 -0600

    se corrige url

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,8 +3,7 @@
 //     public static API_ID = 'eb78bfe7dee043039c9a43b58a943dbd';
 // }
 
-
 export class AppConstants {
-    public static API_BASE_URL = 'https://crudcrud.com/api/';
-    public static API_ID = '2e8a9ff9001d4fa4911ec28231f9865e';
+  public static API_BASE_URL = 'https://crudcrud.com/api';
+  public static API_ID = '2e8a9ff9001d4fa4911ec28231f9865e';
 }
--- src/app/entities/unicorns/unicorns.service.ts
+++ src/app/entities/unicorns/unicorns.service.ts
@@ -1,23 +1,26 @@
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { AppConstants } from './../../app.constants';
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UnicornsService {
-private resourceUrl = AppConstants.API_BASE_URL;
-private resourceID = AppConstants.API_ID;
-public unicornsSubject = new BehaviorSubject<any>([]);
+  private resourceUrl = AppConstants.API_BASE_URL;
+  private resourceID = AppConstants.API_ID;
+  public unicornsSubject = new BehaviorSubject<any>([]);
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  queryUnicorns(): Observable<HttpResponse<any[]>>{
-    return this.http.get<any[]>(this.resourceUrl+'/'+this.resourceID+'/unicorns', {observe: 'response'});
+  queryUnicorns(): Observable<HttpResponse<any[]>> {
+    return this.http.get<any[]>(
+      this.resourceUrl + '/' + this.resourceID + '/unicorns',
+      { observe: 'response' }
+    );
   }
 
-   getUnicorns(): Observable<HttpResponse<any>> {
+  getUnicorns(): Observable<HttpResponse<any>> {
     return this.http
       .get<any>(`${this.resourceUrl}/${this.resourceID}/unicorns`, {
         observe: 'response',
@@ -25,36 +28,45 @@ public unicornsSubject = new BehaviorSubject<any>([]);
       .pipe(
         tap((unicorns) => {
           this.unicornsSubject.next(unicorns.body);
-          console.log(unicorns.body)
+          console.log(unicorns.body);
         })
       );
-      
   }
 
   getUnicorn(id: string): Observable<HttpResponse<any>> {
-    return this.http.get<any[]>(this.resourceUrl+'/'+this.resourceID+'/unicorns/'+id, {observe: 'response'});
+    return this.http.get<any[]>(
+      this.resourceUrl + '/' + this.resourceID + '/unicorns/' + id,
+      { observe: 'response' }
+    );
   }
 
-  postUnicorn(unicorn: any): Observable<HttpResponse<any>>{
+  postUnicorn(unicorn: any): Observable<HttpResponse<any>> {
     let httpHeaders = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     });
-    return this.http.post<any>(this.resourceUrl+'/'+this.resourceID+'/unicorns', unicorn,
-    {headers: httpHeaders, observe: 'response'});
+    return this.http.post<any>(
+      this.resourceUrl + '/' + this.resourceID + '/unicorns',
+      unicorn,
+      { headers: httpHeaders, observe: 'response' }
+    );
   }
 
-  putUnicorn(unicorn: any, id: string): Observable<HttpResponse<any>>{
+  putUnicorn(unicorn: any, id: string): Observable<HttpResponse<any>> {
     let httpHeaders = new HttpHeaders({
-      'Content-Type':'application/json'
+      'Content-Type': 'application/json',
     });
-    delete(unicorn._id);
-    return this.http.put<any>(this.resourceUrl+'/'+this.resourceID+'/unicorns/'+id, unicorn,
-    { headers: httpHeaders, observe: 'response'});
+    delete unicorn._id;
+    return this.http.put<any>(
+      this.resourceUrl + '/' + this.resourceID + '/unicorns/' + id,
+      unicorn,
+      { headers: httpHeaders, observe: 'response' }
+    );
   }
 
-  deleteUnicorn(id: string): Observable<HttpResponse<any>>{
-    return this.http.delete(this.resourceUrl+'/'+this.resourceID+'/unicorns/'+id,
-    {observe: 'response'});
+  deleteUnicorn(id: string): Observable<HttpResponse<any>> {
+    return this.http.delete(
+      this.resourceUrl + '/' + this.resourceID + '/unicorns/' + id,
+      { observe: 'response' }
+    );
   }
-
 }
