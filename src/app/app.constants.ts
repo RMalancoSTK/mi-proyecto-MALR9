543c0c1ec5f51666e84ff11a810e422655a6f018
Rafael Malanco <rafael.malanco@softtek.com>
2023-02-02 13:16:48 -0600

    update

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,6 +3,8 @@
 //     public static API_ID = 'eb78bfe7dee043039c9a43b58a943dbd';
 // }
 
+// Path: src\app\app.module.ts
+
 export class AppConstants {
   public static API_BASE_URL = 'https://crudcrud.com/api';
   public static API_ID = '2e8a9ff9001d4fa4911ec28231f9865e';
